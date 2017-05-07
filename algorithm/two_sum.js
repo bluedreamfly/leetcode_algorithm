6ba320b4dd95211341a88b60f92eab61b620bfb3
hzhwalkbydream <hzhwalkbydream@gmail.com>
2017-05-07 08:17:15 +0800

    add two sum

---
+++ algorithm/two_sum.js
@@ -0,0 +1,32 @@
+/*
+
+description: 给出一个num, 找出两个数的和为target, 并返回两个数对应索引的数组
+examples: 
+  nums = [2, 7, 11, 15], target = 9
+  nums[0] + nums[1] = target
+  return [0, 1]
+*/
+
+
+function findSumTargetIndex(nums, target) {
+	
+	console.log(Array.isArray(nums))
+	if (!Array.isArray(nums)) {
+		throw new Error('nums must be a array');
+	}
+
+	var numMap = {};
+
+	for (let i = 0; i < nums.length; i++) {
+		let otherNum = target - nums[i];
+		if (numMap[otherNum]) {
+			return [numMap[otherNum] - 1, i];
+		}
+    
+    numMap[nums[i]] = i + 1;
+	}
+
+}
+
+
+console.log(findSumTargetIndex([1, 3, 5, 2, 7, 11, 15], 9))
